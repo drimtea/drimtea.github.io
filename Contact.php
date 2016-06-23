c50953658ead1df600736ae7dd88c6b98b8dea41
drimtea <U-English@mail.ru>
2016-06-23 21:46:00 +0300

    Update Contact.php

--- Contact.php
+++ Contact.php
@@ -1,10 +1,10 @@
 <?
 if (array_key_exists('nameFF', $_POST)) {
-   $to = 'U-English@mai.ru';
-   $subject = 'Заполнена контактная форма с '.$_SERVER['HTTP_REFERER'];
+   $to = "U-English@mai.ru";
+   $subject = "Заполнена контактная форма с ".$_SERVER['HTTP_REFERER'];
    $subject = "=?utf-8?b?". base64_encode($subject) ."?=";
    $message = "Имя: ".$_POST['nameFF']."\nEmail: ".$_POST['contactFF']."\nIP: ".$_SERVER['REMOTE_ADDR']."\nТелефон: ".$_POST['telFF']."\nГород: ".$_POST['cityFF'];
-   $headers = 'Content-type: text/plain; charset="utf-8"';
+   $headers = "Content-type: text/plain; charset="utf-8"";
    $headers .= "MIME-Version: 1.0\r\n";
    $headers .= "Date: ". date('D, d M Y h:i:s O') ."\r\n";
    mail($to, $subject, $message, $headers);
